3609883a5032faeea91cb38f2251f2c2139bd243
Tineil_A <tine6573@gmail.com>
2025-01-31 14:07:39 -0500

    Completed for loop and break sections of lesson

--- lesson-4/js/loops.js
+++ lesson-4/js/loops.js
@@ -1,9 +1,18 @@
 let courses = ['Intro to Web Programming', 'Client-Side JavaScript', 'Bitmap & Vector Graphics', 'Web UI Design with CSS', 'Relational Database'];
-let courseList = 'My courses this term are ';
+let courseList = 'My courses this term are: ';
 const msg = document.querySelector('#courses');
 /* STEP 1a: A simple FOR loop
 A simple loop of this structure needs an initializer (counter variable), an exit conditional, and and incrementor/decrementor */
-
+for (let i = 0; i < courses.length; i ++) {
+	//check to see if we are on the last course in the list
+	if (i === courses.length - 1){
+		// finish the sentence
+		courseList += `and ${courses[i]}.`
+	} else {
+		// add each course to the sentence
+	courseList += `${courses[i]}, `;	
+	}
+};
 msg.textContent = courseList;
 // STEP 1b: Add a conditional for the last item in the array so we can finish the sentence with a period.
 
@@ -14,13 +23,23 @@ const input = document.querySelector('input');
 const btn = document.querySelector('button#cities');
 
 btn.addEventListener('click', function() {
-	var searchName = input.value;
+	let searchName = input.value;
+	//console.log(searchName)
 	input.value = '';
 	input.focus();
 	/* STEP 2a: Create a FOR loop to iterate through the cities array */
-
+	for (let i = 0; i < cities.length; i ++){
+		//console.log(cities[i]);
 		/* STEP 2b: Build an IF/ELSE conditional that compares searchName with cities[i] */
-
+		if (searchName === cities[i]){
+			// message that the city in the search was in the array
+			result.textContent = `${searchName} is in the cities array.`;
+			// We found the city so stop looking and exit the loop
+			break;
+		} else{
+			result.textContent = `${searchName} is NOT in the cities array.`;
+		};
+	};
 });
 
 
@@ -31,10 +50,10 @@ let num = 50;
 for (let i = 1; i <= num; i ++) {
 	let sqRoot = Math.sqrt(i);
 	/* STEP 3a: Build an IF statement that checks whether the square root of the number is NOT an integer */
-	//if () {
+	if () {
 		/* STEP 3b: If the square root of the number is not an integer, we don't want to output it to the paragraph, so skip the rest of the instructions inside this loop and go back up to the FOR, using 'continue' */
 
-	//}
+	}
 	numberList.textContent += i + ' ';
 }
 
