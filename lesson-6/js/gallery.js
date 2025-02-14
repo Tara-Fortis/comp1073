f12cf4f1bc0e6b149582110f6ff84de73ff0107b
Tineil_A <tine6573@gmail.com>
2025-02-14 15:11:34 -0500

    completed gallery lesson

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -1,16 +1,24 @@
 // STEP 1: Initialize and declare variables
-
-
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar= document.querySelector(".thumb-bar");
 /* STEP 2: Loop 5 times to create the <img> elements */
-
+for (let i = 1; i < 6; i++){
 		/* STEP 3a: Create a new DOM node - an image element */
-
+		const newImage = document.createElement("img");
 		/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
-
+		newImage.setAttribute("src", `Images/pic${i}.jpg`);
+		// console.log(newImage);
 		/* Append the new image element to the thumbBar div, named in STEP 1 */
-
+		thumbBar.append(newImage);
 		/* STEP 3c: Build event handler for each <img> */
-
+		newImage.addEventListener("click", function(event){
+			// console.log(event.target);
+			let imageSrc = event.target.getAttribute("src");
+			// console.log(imageSrc);
+			// set main image to the src of the clicked thumbnail
+			displayedImage.setAttribute("src", imageSrc);
+		})
+}
 
 /* STEP 4: Function to change the src of the main <img> */
 
